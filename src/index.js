06a5be3db40f38ecb0c17eb903795cc3605b08f1
vannakvy <vannakvy2020@gmail.com>
2021-07-25 15:06:19 +0700

    first inti

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import './static/index.css';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
